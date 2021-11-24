87b243a9aba82823c809f6f76e835b18fc35077f
Miguel Moreno Baladron <miguel@recast.app>
2021-11-24 12:26:12 +0000

    Add event

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,20 @@
 interface IButtonProps {
     color: string,
-    text: string
+    text: string,
+    onClick: Function
 }
 
 const Button = ({color, text}: IButtonProps) => {
+    
+    const onClick = () => {}
+
     return (
-        <button style={{backgroundColor: color}} className='btn'>Add</button>
+        <button onClick={onClick}style={{backgroundColor: color}} className='btn'>Add</button>
     )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
 }
 
 export default Button;
