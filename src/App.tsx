a91b9ccf98a3848c9ba92236e87422a952037ced
Miguel Moreno Baladron <miguel@recast.app>
2021-11-24 14:38:32 +0000

    Toggle reminder

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,56 @@ import Tasks from "./components/Tasks";
 import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
 
-  const [tasks, setTasks] = useState(
-    [
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true
-        },
-        {
-            id: 2,
-            text: 'Meeting at School',
-            day: 'Feb 6th at 1:30pm',
-            reminder: true
-        },
-        {
-            id: 3,
-            text: 'Food Shopping',
-            day: 'Feb 5th at 2:30pm',
-            reminder: false
-        }
-    
-    ]
-);
+  // Delete task
 
+  const deleteTask = (id: number) => {
+    // console.log('delete ', id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
-// Delete task
+  // Toggle reminder
 
-const deleteTask = (id: number) => {
-  // console.log('delete ', id);
-  setTasks(tasks.filter(task => task.id !== id));
-}
+  const toggleReminder = (id: number) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
-    <div className='container'>
-      <Header title='Hello'/>
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}></Tasks>: 'No tasks to show'}
+    <div className="container">
+      <Header title="Hello" />
+      {tasks.length > 0 ? (
+        <Tasks
+          onToggle={toggleReminder}
+          tasks={tasks}
+          onDelete={deleteTask}
+        ></Tasks>
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   );
 }
