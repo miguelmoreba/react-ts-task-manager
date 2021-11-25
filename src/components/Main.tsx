b8a403ff77129e1770f3b76090e7bf93bd376fdd
Miguel Moreno Baladron <miguel@recast.app>
2021-11-25 17:39:26 +0000

    Get tasks from server

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,39 +4,22 @@ import AddTask from "./AddTask";
 import axios from "axios";
 
 const Main = ({ showAddTask }) => {
+
+  const [tasks, setTasks] = useState<any[]>([]);
+
   useEffect(() => {
     axios.get("http://localhost:5000/tasks").then((res) => {
       console.log(res.data);
+      setTasks(res.data);
     });
   }, []);
-
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "Feb 5th at 2:30pm",
-      reminder: false,
-    },
-  ]);
 
   // Add Task
 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 1000) + 1;
     const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+    tasks.length > 0 && setTasks([...tasks, newTask]);
   };
 
   // Delete task
