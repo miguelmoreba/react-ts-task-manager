9aa549beb6e7492fda304563e924b4c72f2b93e0
Miguel Moreno Baladron <miguel@recast.app>
2021-11-25 16:26:26 +0000

    Refactor to make use of routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,28 @@
+import Footer from "./components/Footer";
+import About from "./components/About";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Tasks from "./components/Tasks";
-import Footer from "./components/Footer"
 import { useState } from "react";
-import AddTask from "./components/AddTask";
+import Main from "./components/Main";
 
 function App() {
-
   const [showAddTask, setShowAddTask] = useState(false);
 
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "Feb 5th at 2:30pm",
-      reminder: false,
-    },
-  ]);
-
-  // Add Task
-
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1;
-    const newTask = {id, ...task}
-    setTasks([...tasks, newTask]);
-  }
-
-
-  // Delete task
-  const deleteTask = (id: number) => {
-    // console.log('delete ', id);
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
-  // Toggle reminder
-
-  const toggleReminder = (id: number) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
-      )
-    );
-  };
-
   return (
-    <div className="container">
-      <Header showAddTask={showAddTask} onAdd={() => setShowAddTask(!showAddTask)} title="Task Tracker" />
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? (
-        <Tasks
-          onToggle={toggleReminder}
-          tasks={tasks}
-          onDelete={deleteTask}
-        ></Tasks>
-      ) : (
-        "No tasks to show"
-      )}
-      <Footer/>
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+          showAddTask={showAddTask}
+          onAdd={() => setShowAddTask(!showAddTask)}
+          title="Task Tracker"
+        />
+        <Routes>
+          <Route path="/" element={<Main showAddTask={showAddTask} />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Main.tsx
@@ -0,0 +1,69 @@
+import Tasks from "./Tasks";
+import { useState } from "react";
+import AddTask from "./AddTask";
+
+const Main = ({showAddTask}) => {
+  
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
+
+  // Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  // Delete task
+  const deleteTask = (id: number) => {
+    // console.log('delete ', id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle reminder
+
+  const toggleReminder = (id: number) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  return (
+    <>
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks
+          onToggle={toggleReminder}
+          tasks={tasks}
+          onDelete={deleteTask}
+        ></Tasks>
+      ) : (
+        "No tasks to show"
+      )}
+    </>
+  );
+};
+
+export default Main;
